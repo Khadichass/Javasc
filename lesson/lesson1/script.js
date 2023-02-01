105a5ebd269e568fabc90bf1b0ed67a55421d527
Khadicha <246abdurashidova123@gmail.com>
2023-02-01 18:09:52 +0500

    commit 3

--- lesson/lesson1/script.js
+++ lesson/lesson1/script.js
@@ -1,1 +1,10 @@
-alert("Hello World");
+// alert("Hello World");
+
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
