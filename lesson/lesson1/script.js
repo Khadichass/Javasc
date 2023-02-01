31cefc26b143c73e272d0a37933b8a553fa4edf7
Khadicha <246abdurashidova123@gmail.com>
2023-02-01 19:09:39 +0500

    commit 4

--- lesson/lesson1/script.js
+++ lesson/lesson1/script.js
@@ -8,3 +8,27 @@ console.log("Hello World");
 console.log("Hello World");
 console.log("Hello World");
 console.log("Hello World");
+
+// Data types || Variable and values || let var const
+
+let Name = "ism";
+console.log(Name);
+
+let width = 50;
+console.log(width);
+
+let height;
+console.log(height);
+
+let data = null;
+
+console.log(data);
+
+let first = true;
+console.log(first);
+
+let second = false;
+console.log(second);
+
+let all = `${Name} ${width} `;
+console.log(all);
