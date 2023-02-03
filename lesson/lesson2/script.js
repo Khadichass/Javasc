f234df79e3306319a59340383aa0a26e5960edd2
Khadicha <246abdurashidova123@gmail.com>
2023-02-03 19:22:11 +0500

    commit 13

--- lesson/lesson2/script.js
+++ lesson/lesson2/script.js
@@ -1,5 +1,21 @@
 "use script";
+//first task
 
 let year = Number(prompt("How old are you?"));
 let num = 2023;
 console.log(2023 - year);
+
+//second task
+let a = Number(prompt("a="));
+let b = Number(prompt("b="));
+let c = Number(prompt("c="));
+let d = Number(prompt("d="));
+console.log(((a + b) / d) * c);
+
+//third task
+let e = Number(prompt("e="));
+let f = Number(prompt("f="));
+let g = 2;
+let h = 3;
+
+console.log(((a + b) ** h / (d + c) ** g + e) * f);
