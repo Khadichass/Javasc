d3457a4af96ed906a0bd59db966eb733bf545675
Khadicha <246abdurashidova123@gmail.com>
2023-02-03 14:55:49 +0500

    commit 11

--- tasks/homework1/script.js
+++ tasks/homework1/script.js
@@ -1,2 +1,45 @@
 let intro = "Hello World";
 console.log(intro);
+
+let word1 = "Welcome";
+console.log(word1);
+
+let word2 = 30;
+console.log(word2);
+
+let word3 = "days";
+console.log(word3);
+
+let word4 = "JavaScript";
+console.log(word4);
+
+let all = `${word1} to ${word2} ${word3} of ${word4}`;
+console.log(all);
+
+let flower;
+console.log(flower);
+
+let number;
+console.log(number);
+
+let name = "jane";
+console.log(name);
+
+let raqam = 40;
+console.log(typeof raqam);
+
+let data = null;
+
+console.log(data);
+
+let first = true;
+console.log(first);
+
+let second = false;
+console.log(second);
+
+let symbl = Symbol(1234);
+console.log(symbl);
+
+const triangle = 30;
+console.log(triangle);
