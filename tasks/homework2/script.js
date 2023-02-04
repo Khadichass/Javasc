23210c31ce83be57c26a9ed81abfec7f5c02fdab
Khadicha <246abdurashidova123@gmail.com>
2023-02-04 20:40:24 +0500

    commit 15

---
+++ tasks/homework2/script.js
@@ -0,0 +1,20 @@
+"use script";
+//task 1
+
+let weight = Number(prompt("Enter your weight:"));
+let height = Number(prompt("Enter your height:"));
+let A = weight / height ** 2;
+
+if (A < 18.5) {
+  console.log(underweight);
+} else if (18.5 < A < 24.9) {
+  console.log(normal);
+} else if (25 < A < 29.9) {
+  console.log(overweight);
+} else if (30 < A < 34.9) {
+  console.log(obese);
+} else if (35 < A) {
+  console.log(extremely_obese);
+}
+
+//task 2
