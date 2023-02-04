2beb22a745e1a3b22860128f8fbf58a46744735c
Khadicha <246abdurashidova123@gmail.com>
2023-02-04 21:01:05 +0500

    commit 16

--- tasks/homework2/script.js
+++ tasks/homework2/script.js
@@ -1,20 +1,28 @@
 "use script";
 //task 1
 
-let weight = Number(prompt("Enter your weight:"));
-let height = Number(prompt("Enter your height:"));
-let A = weight / height ** 2;
+// let weight = Number(prompt("Enter your weight:"));
+// let height = Number(prompt("Enter your height:"));
+// let A = weight / height ** 2;
 
-if (A < 18.5) {
-  console.log(underweight);
-} else if (18.5 < A < 24.9) {
-  console.log(normal);
-} else if (25 < A < 29.9) {
-  console.log(overweight);
-} else if (30 < A < 34.9) {
-  console.log(obese);
-} else if (35 < A) {
-  console.log(extremely_obese);
+// if (A < 18.5) {
+//   console.log(underweight);
+// } else if (18.5 < A < 24.9) {
+//   console.log(normal);
+// } else if (25 < A < 29.9) {
+//   console.log(overweight);
+// } else if (30 < A < 34.9) {
+//   console.log(obese);
+// } else if (35 < A) {
+//   console.log(extremely_obese);
+// }
+
+// task 2
+
+let num = Number(prompt("Enter a number:"));
+
+if (num % 2) {
+  console.log(0);
+} else {
+  console.log(1);
 }
-
-//task 2
