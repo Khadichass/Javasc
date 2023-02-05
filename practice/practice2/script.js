f071b33fa638ba53b9e957af04c29f680fbc8d2d
Khadicha <246abdurashidova123@gmail.com>
2023-02-05 13:02:22 +0500

    commit 22

---
+++ practice/practice2/script.js
@@ -0,0 +1,31 @@
+"use script";
+
+// Butun sonlarga oid masalalar
+
+// 1-masala
+
+// let L = Number(prompt("santimetr:"));
+// let M = (L - (L % 100)) / 100;
+// alert(M);
+
+// 2-masala
+
+// let M = Number(prompt("kg:"));
+// let T = (M - (M % 1000)) / 1000;
+// alert(T);
+
+// 3-masala
+
+// let B = Number(prompt("Kilobayts:"));
+// let KB = (B - (B % 1024)) / 1024;
+// alert(KB);
+
+// 4-masala
+
+// let C = A > B;
+
+let A = Number(prompt("A:"));
+let B = Number(prompt("B:"));
+
+let D = (A - (A % B)) / B;
+alert(D);
