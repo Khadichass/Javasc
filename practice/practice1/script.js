d53fabdb7d06301f497679d427751d9494fdb1d1
Khadicha <246abdurashidova123@gmail.com>
2023-02-05 12:30:14 +0500

    commit 19

---
+++ practice/practice1/script.js
@@ -0,0 +1,92 @@
+"use script";
+
+//Kirish, chiqarish va o'zlashtirish operatorlariga oid masalalar
+
+// 1-masala
+
+// let a = Number(prompt("tomon:"));
+
+// let P = 4 * a;
+// alert(P);
+
+// 2-masala
+
+// let a = Number(prompt("kvadrat tomoni:"));
+// let S = a ** 2;
+// alert(S);
+
+// 3-masala
+
+// let a = Number(prompt("a tomon:"));
+// let b = Number(prompt("b tomon:"));
+// let S = a * b;
+// alert(S);
+
+// let P = 2 * (a + b);
+// alert(P);
+
+// 4-masala
+
+// let p = 3.14;
+// let d = Number(prompt("d tomon:"));
+// let L = p * d;
+// alert(L);
+
+// 5-masala
+
+// let a = Number(prompt("a tomon:"));
+// let V = a ** 3;
+// let S = 6 * a ** 2;
+// alert(V);
+// alert(S);
+
+// 6-masala
+
+// let a = Number(prompt("a tomon:"));
+// let b = Number(prompt("b tomon:"));
+// let c = Number(prompt("c tomon:"));
+
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+
+// alert(V);
+// alert(S);
+
+// 7-masala
+
+// let p = 3.14;
+// let R = Number(prompt("Radius:"));
+// let L = 2 * p * R;
+// let S = p * R ** 2;
+// alert(L);
+// alert(S);
+
+// 8-masala
+
+// let a = Number(prompt("a son:"));
+// let b = Number(prompt("b son:"));
+// let Average = (a + b) / 2;
+// alert(Average);
+
+// 9-masala
+
+// let a = Number(prompt("a son:"));
+// let b = Number(prompt("b son:"));
+// let I = (a * b) ** 1 / 2;
+// alert(I);
+
+// 10-masala
+
+// let a = Number(prompt("a son:"));
+// let b = Number(prompt("b son:"));
+// let Y = a + b;
+// let K = a * b;
+// let Kv1 = a ** 2;
+// let Kv2 = b ** 2;
+// alert(Y);
+// alert(K);
+// alert(Kv1);
+// alert(Kv2);
+
+// let a = Number(prompt("a son:"));
+// alert(Math.abs(a));
