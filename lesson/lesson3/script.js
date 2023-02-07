d768e7e21ea6c951ca835a43a15959c01a73a818
Khadicha <246abdurashidova123@gmail.com>
2023-02-07 22:58:12 +0500

    commit 41

---
+++ lesson/lesson3/script.js
@@ -0,0 +1,41 @@
+"use script";
+
+// let a = true;
+// let b = false;
+
+// if (a) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+// if (!b) {
+//   console.log(a);
+// } else {
+//   console.log(a);
+// }
+
+// if (b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+// if (a && b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+// if (a || b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+let a = Number(prompt("a number:"));
+let b = Number(prompt("b number:"));
+
+a < 50 && b > 20
+  ? console.log("you are adult")
+  : console.log("you are teenager");
