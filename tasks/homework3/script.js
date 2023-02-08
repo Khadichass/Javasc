e4018f7a72cc9dbc1c9cdf0317124845646d5c32
Khadicha <246abdurashidova123@gmail.com>
2023-02-08 17:33:13 +0500

    commit 51

--- tasks/homework3/script.js
+++ tasks/homework3/script.js
@@ -1,35 +1,31 @@
 " use script";
 // task 1
 
-// let dolphins1 = Number(prompt("dolphins1 :"));
-// let dolphins2 = Number(prompt("dolphins2 :"));
-// let dolphins3 = Number(prompt("dolphins3 :"));
-// let koalas1 = Number(prompt("koalas1 :"));
-// let koalas2 = Number(prompt("koalas2 :"));
-// let koalas3 = Number(prompt("koalas3 :"));
+let dolphins1 = Number(prompt("dolphins1 :"));
+let dolphins2 = Number(prompt("dolphins2 :"));
+let dolphins3 = Number(prompt("dolphins3 :"));
+let koalas1 = Number(prompt("koalas1 :"));
+let koalas2 = Number(prompt("koalas2 :"));
+let koalas3 = Number(prompt("koalas3 :"));
 
-// let overalld = dolphins1 + dolphins2 + dolphins3;
-// let overallk = koalas1 + koalas2 + koalas3;
+let overalld = dolphins1 + dolphins2 + dolphins3;
+let overallk = koalas1 + koalas2 + koalas3;
 
-// if (overalld >= 100) {
-//   alert("100 balldan baland");
-// } else {
-//   alert("kubok berilmaydi");
-// }
-
-// if (overallk >= 100) {
-//   alert("100 balldan baland");
-// } else {
-//   alert("kubok berilmaydi");
-// }
+if (overalld > overallk && overalld >= 100) {
+} else if (overalld < overallk && overalld >= 100) {
+}
 
 // task 2
 
 // let login = prompt("Loginni kiriting :");
 
-// if ((login = "admin")) {
-//   alert((let = prompt("Admin parolni kiriting :")));
-// } else if ((login = "User")) {
+// if (login == "admin") {
+//   let admin = prompt("Admin parolni kiriting :");
+//   // alert("Admin parolini kiriting");
+//   if (admin == 12345) {
+//     alert("Tizimga admin sifatida kirdingiz");
+//   }
+// } else if (login == "user") {
 //   alert((let = prompt("User parolni kiriting :")));
 // }
 
