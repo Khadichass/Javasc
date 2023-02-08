0fdf4d816b30da8be507110b1352d948d0a1496f
Khadicha <246abdurashidova123@gmail.com>
2023-02-08 19:27:32 +0500

    commit 53

---
+++ lesson/lesson5fn/script.js
@@ -0,0 +1,62 @@
+"use script";
+
+// functions
+
+// declaration || expression || arrow  function
+
+let a = Number(prompt("a :"));
+
+let plus;
+let minus;
+
+// iffunction(a);
+
+// function iffunction(a) {
+//   if (a > 0) {
+//     console.log(a + 1);
+//   } else if (a < 0) {
+//     console.log(a - 2);
+//   }
+// }
+
+// const iffunction = function (a) {
+//   if (a > 0) {
+//     console.log(a + 1);
+//   } else if (a < 0) {
+//     console.log(a - 2);
+//   }
+// };
+
+// iffunction(a);
+
+// const iffunction = (a) => {
+//   if (a > 0) {
+//     console.log(a + 1);
+//   } else if (a < 0) {
+//     console.log(a - 2);
+//   }
+// };
+
+// iffunction(a);
+
+// const iffunction = (a) => {
+//   if (a > 0) {
+//     console.log(a + 1);
+//   } else if (a < 0) {
+//     console.log(a);
+//   }
+// };
+
+// iffunction(a);
+
+// const iffunction = (a) => {
+//   if (a > 0) {
+//     console.log(a + 1);
+//   } else if (a < 0) {
+//     console.log(a - 2);
+//   } else if (a == 0) {
+//     console.log((a = 10));
+//   }
+// };
+
+// iffunction(a);
