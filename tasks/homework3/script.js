77df2ea6c5a3f116c17e17fe496d15cea2cbffbd
Khadicha <246abdurashidova123@gmail.com>
2023-02-08 17:03:59 +0500

    commit 50

--- tasks/homework3/script.js
+++ tasks/homework3/script.js
@@ -1,1 +1,47 @@
 " use script";
+// task 1
+
+// let dolphins1 = Number(prompt("dolphins1 :"));
+// let dolphins2 = Number(prompt("dolphins2 :"));
+// let dolphins3 = Number(prompt("dolphins3 :"));
+// let koalas1 = Number(prompt("koalas1 :"));
+// let koalas2 = Number(prompt("koalas2 :"));
+// let koalas3 = Number(prompt("koalas3 :"));
+
+// let overalld = dolphins1 + dolphins2 + dolphins3;
+// let overallk = koalas1 + koalas2 + koalas3;
+
+// if (overalld >= 100) {
+//   alert("100 balldan baland");
+// } else {
+//   alert("kubok berilmaydi");
+// }
+
+// if (overallk >= 100) {
+//   alert("100 balldan baland");
+// } else {
+//   alert("kubok berilmaydi");
+// }
+
+// task 2
+
+// let login = prompt("Loginni kiriting :");
+
+// if ((login = "admin")) {
+//   alert((let = prompt("Admin parolni kiriting :")));
+// } else if ((login = "User")) {
+//   alert((let = prompt("User parolni kiriting :")));
+// }
+
+// task 3
+
+// let price = Number(prompt("check price :"));
+
+// let A = (price * 115) / 100;
+// let B = (price * 12) / 10;
+
+// if (50 < price < 300) {
+//   alert(A);
+// } else {
+//   alert(B);
+// }
