e6c46888b2e4cab188d2f447f37a033dbbb43342
Khadicha <246abdurashidova123@gmail.com>
2023-02-10 16:12:36 +0500

    commit 60

--- practice/practice3/script.js
+++ practice/practice3/script.js
@@ -1,1 +1,174 @@
 "use script";
+
+//1-MASALA
+/*let a =Number(prompt("Sonni kiriting:"));
+let b=(a>0);
+alert(b);
+let d=(a=1)
+alert(d);*/
+
+//2-MASALA
+/*let a =Number(prompt("musbat son:"));
+let b=Number(prompt("manfiy son:"))
+if(a>0){
+  let b=(a=1);
+  alert(b);
+}else if(0>a)  {
+  let d=(a-2);
+  alert(d);
+}*/
+
+//3-MASALA
+/*let a =Number(prompt("Sonni kiriting:"));
+let b= Number(prompt("manfiy son:"));
+let c=Number(prompt("butun son:"));
+if(a>0){
+  let e=(a=1)
+  console.log(e);
+} else if(a<0){
+  let f=(a=-2);
+  console.log(f);
+}else if(a=0){
+  let i=(a=0);
+  console.log(i);
+}*/
+
+//4-MASALA
+/*let a =Number(prompt("Sonni kiriting(A):"));
+let b =Number(prompt("Sonni kiriting(B):"));
+let c =Number(prompt("Sonni kiriting(C):"));
+
+  let musbatSum;
+  if(a>0 && b>0 && c>0){
+    musbatSum=3;
+  }else if(a>0 && b>0 && c<0){
+    musbatSum=2
+  }*/
+
+//6-MASALA
+/*let a =Number(prompt("Sonni kiriting(A):"));
+let b =Number(prompt("Sonni kiriting(B):"));
+if(a>b){
+//console.log(a);
+alert(a);
+}else if(a<b){
+  //console.log(b);
+  alert(b);
+}*/
+
+//7-MASALA
+/*let d =Number(prompt("Sonni kiriting(A):"));
+let c =Number(prompt("Sonni kiriting(B):"));
+let a=11;
+let b=12;
+if(a<b){
+  //console.log(${b},2);
+  alert(a);
+  }else if(a>b){
+    //console.log(${a},1);
+    alert(b);
+  }*/
+
+//8-MASALA
+/*let a =Number(prompt("Sonni kiriting(A):"));
+let b =Number(prompt("Sonni kiriting(B):"));
+if(a>b){
+  alert(a);
+  alert(b);
+}else if(a<b){
+  alert(b);
+}*/
+
+//9-MASALA
+/*let a =Number(prompt("Sonni kiriting(A):"));
+let b =Number(prompt("Sonni kiriting(B):"));
+if(a<b){
+  alert(a);
+  alert(b);
+}else if(a<b){
+  alert(a);
+}*/
+
+//12-MASALA
+/*let a =Number(prompt("Sonni kiriting(A):"));
+let b =Number(prompt("Sonni kiriting(B):"));
+let c=Number(prompt("Sonni kiriting(C):"));
+if(a<b){
+  alert(a);
+}else if(a=b){alert(b)
+}else if(a>b){alert(kichik)}*/
+
+//13-MASALA
+/*let a =Number(prompt("Sonni kiriting(A):"));
+let b =Number(prompt("Sonni kiriting(B):"));
+let c=Number(prompt("Sonni kiriting(C):"));
+if(a=b){
+  alert(a);
+}else if(a>b){
+  alert(b);
+}else if(a<b){
+  alert(c);
+}*/
+
+//14-MASALA
+/*let a =Number(prompt("Sonni kiriting(A):"));
+let b =Number(prompt("Sonni kiriting(B):"));
+let c=Number(prompt("Sonni kiriting(C):"));
+if(a<b){
+  alert(a);
+  alert(b);
+}else if(a>b){
+  alert(b);
+  alert(a);
+}else if(a>c && b>c){
+  alert(a=c);
+  alert(b=c);
+}*/
+
+//15-MASALA
+/*let a =Number(prompt("A:"));
+let b =Number(prompt("B:"));
+let c=Number(prompt("C:"));
+let d=(a,b,c);
+alert(d);
+let e=(a+b+c);
+alert(e);*/
+
+//16-MASALA
+/*let a =Number(prompt("A-"));
+let b =Number(prompt("B-"));
+let c=Number(prompt("C-"));
+if(a>b,b>c){
+  let a=(a*2);
+alert(a);
+let b=(b*2)
+alert(b);
+let c=(c*2);
+alert (c);
+}else if(a<b,b<c){
+  let a=(a*-2);
+alert(a);
+let b=(b*-2)
+alert(b);
+let c=(c*-2);
+alert (c);
+}*/
+
+//17-MASALA
+/*let a =Number(prompt("A-"));
+let b =Number(prompt("B-"));
+let c=Number(prompt("C-"));
+if(a>b,b>c && a<b,b<c){
+  let a=(a*2);
+alert(a);
+let b=(b*2)
+alert(b);
+let c=(c*2);
+alert (c);
+}else if(a<b,b<c && a>b,b>c){
+  let a=(a*-2);
+alert(a);
+let b=(b*-2)
+alert(b);
+let c=(c*-2);
+alert (c);}*/
