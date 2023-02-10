22fa411885256cd4a6263f5dfe6372a7fd9ac023
Khadicha <246abdurashidova123@gmail.com>
2023-02-10 17:25:06 +0500

    commit 63

--- tasks/homework4/script.js
+++ tasks/homework4/script.js
@@ -2,33 +2,6 @@
 
 // task 1
 
-// let a = Number(prompt("a :"));
-// let c = prompt("Choose operator :");
-// let b = Number(prompt("b :"));
-
-// let output;
-
-// switch (c) {
-//   case "*":
-//     output = a * b;
-//     break;
-//   case "/":
-//     output = a / b;
-//     break;
-//   case "+":
-//     output = a + b;
-//     break;
-//   case "-":
-//     output = a - b;
-//     break;
-//   default:
-//     output = "topilmadi";
-// }
-
-// console.log(output);
-
-// task 2
-
 let a = Number(prompt("Birinchi sonni kirirting :"));
 let b = Number(prompt("Ikkinchi sonni kirirting :"));
 let c = prompt("Qaysi operatorni tanlaysiz :");
