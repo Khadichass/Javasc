e741989731bdb3ff529dfea27a4d01caaef5b02a
Khadicha <246abdurashidova123@gmail.com>
2023-02-10 16:08:14 +0500

    commit 58

--- practice/practice2/script.js
+++ practice/practice2/script.js
@@ -96,3 +96,179 @@
 
 // let G = B + D + F;
 // alert(G);
+
+//12-MASALA
+/*let a =Number(prompt("raqamlar yigindisi:"));
+let b=(a/100);
+console.log(b);
+let c=(a+b);
+console.log(c);
+let d=(a/10);
+console.log(d);
+let e=((c-d)*10)
+let f=((b+d)*10 + (e/100));
+console.log(f);*/
+
+//13-MASALA
+/*let a =Number(prompt("raqamlar yigindisi:"));
+let b=(a/100);
+console.log(b);
+let c=(a+b);
+console.log(c);
+let d=(a/10);
+console.log(d);
+let e=((c-d)*10)
+console.log(e);
+let f=((b+100)*c +(e/10));
+console.log(f);*/
+
+//14-MASALA
+/*let a =Number(prompt("raqamlar yigindisi:"));
+let b=(a/100);
+console.log(b);
+let c=(a+b);
+console.log(c);
+let d=(a/10);
+console.log(d);
+let e=((c-d)*10)
+console.log(e);
+let f=((b+10)*d +(e*100))
+console.log(f);*/
+
+//15-MASALA
+/*let a =Number(prompt("raqamlar yigindisi:"));
+let b=(a/100);
+alert(b);
+let c=(a+b);
+alert(c);
+let d=(a/10);
+alert(d);
+let e=((c-d)*10)
+alert(e);
+let f =((b*10+d)(e+100));
+alert(f);*/
+
+//16-MASALA
+/*let a =Number(prompt("raqamlar yigindisi:"));
+let b=(a/100);
+console.log(b);
+let c=(a+b);
+console.log(c);
+let d=(a/10);
+console.log(d);
+let e=((c-d)*10)
+console.log(e);
+let f=((b+100*d)(e+10));
+console.log(f);*/
+
+//17-MASALA
+/*let a =Number(prompt("yuzliklar xonasi:"));
+let b =(a/1234);
+console.log(b);
+let c =(a*1234);
+console.log(c);
+let d =(c/100);
+console.log(d);
+let e=((d-c)*100);
+console.log(e);
+let f=((c+d)*100)
+console.log(f);*/
+
+//17-MASALA
+/*let a =Number(prompt("17-masala:"));
+let b =(a/1000);
+alert (b);
+let c=(a%1000);
+alert(c);*/
+
+//18-MASALA
+/*let a =Number(prompt("sonni kiriting:"));
+let b =(a/1000);
+alert (b);
+let c=(a*1000);
+alert(c);*/
+
+//19-MASALA
+/*let a =Number(prompt("minut:"));
+let b=(a/3600);
+//let b=(a/60);
+alert (b);*/
+
+//20-MASALA
+/*let a =Number(prompt("soat:"));
+//let b=(a/3600);
+let b=(a/60);
+alert (b);*/
+
+//21-MASALA
+/*let a =Number(prompt("minut-sekunt:"));
+let b=(a/3600);
+//let b=(a/60);
+alert (b);
+let c=((a-b)*3600);
+alert(c);*/
+
+//22-MASALA
+/*let a =Number(prompt("soat-sekunt:"));
+let b=(a/60);
+//let b=(a/60);
+alert (b);
+let c=((a-b)*60);
+alert(c);*/
+
+//23-MASALA
+/*let a =Number(prompt("to'rttasi birda:"));
+let b=(a/60);
+//let b=(a/60);
+alert (b);
+let c=((a-b)*60);
+alert(c);
+let d=(a/3600);
+//let b=(a/60);
+alert (d);
+let e=((d-c)*3600);
+alert(e);*/
+
+//24-28-MASALA
+/*let day =Number(prompt("choose the week days:"));
+let output;
+switch (day){
+  case 0:
+    output="Yakshanba"
+    break;
+    case 1:
+      output ="Dushanba"
+      break;
+      case 2:
+        output="Seshanba"
+        break;
+        case 3:
+          output="Chorshanba"
+          break;
+          case 4:
+          output="Payshanba"
+          break;
+          case 5:
+            output="Jum'a"
+            break;
+            case 6:
+              output="Shanba"
+              break;
+}
+alert(${output});*/
+
+//29-MASALA
+/*let a=Number(prompt("a-tomon"));
+let b=Number(prompt("b-tomon"));
+let c=Number(prompt("c-tomon"));
+let d=((a*b)/(c*c));
+alert(d);
+let e=((a*b)/(c*c)-d);
+alert(e);*/
+
+//30-MASALA
+/*let a=Number(prompt("unkown year:"));
+let b=(a/100);
+alert(b);
+let d=(b+1);
+alert(d);*/
