06565d567b80c99b0b48b6ee1c1bcd42dc901b35
Khadicha <246abdurashidova123@gmail.com>
2023-02-13 16:24:06 +0500

    commit 70

---
+++ practice/practice4/script.js
@@ -0,0 +1,330 @@
+"use script";
+
+// 1-masala
+
+// let a = Number(prompt("enter the number :"));
+// let output;
+// switch (a) {
+//   case 1:
+//     output = "Dushanba";
+//     break;
+//   case 2:
+//     output = "Seshanba";
+//     break;
+//   case 3:
+//     output = "Chorshanba";
+//     break;
+//   case 4:
+//     output = "Payshanba";
+//     break;
+//   case 5:
+//     output = "Juma";
+//     break;
+//   case 6:
+//     output = "Shanba";
+//     break;
+//   case 7:
+//     output = "Yakshanba";
+//     break;
+// }
+
+// alert(output);
+
+// 2-masala
+
+// let k = Number(prompt("enter the number :"));
+// let output;
+
+// switch (k) {
+//   case 1:
+//     output = "yomon";
+//     break;
+//   case 2:
+//     output = "qoniqarsiz";
+//     break;
+//   case 3:
+//     output = "qoniqarli";
+//     break;
+//   case 4:
+//     output = "yaxshi";
+//     break;
+//   case 5:
+//     output = "a'lo";
+//     break;
+//   default:
+//     output = "xato";
+//     break;
+// }
+
+// alert(output);
+
+// 3-masala
+
+// let a = Number(prompt("Enter the month number :"));
+// let output;
+
+// switch (a) {
+//   case 1:
+//     output = "qish";
+//     break;
+//   case 2:
+//     output = "qish";
+//     break;
+//   case 3:
+//     output = "bahor";
+//     break;
+//   case 4:
+//     output = "bahor";
+//     break;
+//   case 5:
+//     output = "bahor";
+//     break;
+//   case 6:
+//     output = "yoz";
+//     break;
+//   case 7:
+//     output = "yoz";
+//     break;
+//   case 8:
+//     output = "yoz";
+//     break;
+//   case 9:
+//     output = "kuz";
+//     break;
+//   case 10:
+//     output = "kuz";
+//     break;
+//   case 11:
+//     output = "kuz";
+//     break;
+//   case 12:
+//     output = "qish";
+//     break;
+// }
+
+// alert(output);
+
+// 4-masala
+
+// let a = Number(prompt("Enter the month number :"));
+// let output;
+
+// switch (a) {
+//   case 1:
+//     output = "31 kun";
+//     break;
+//   case 2:
+//     output = "28 kun";
+//     break;
+//   case 3:
+//     output = "31 kun";
+//     break;
+//   case 4:
+//     output = "30 kun";
+//     break;
+//   case 5:
+//     output = "31 kun";
+//     break;
+//   case 6:
+//     output = "30 kun";
+//     break;
+//   case 7:
+//     output = "31 kun";
+//     break;
+//   case 8:
+//     output = "31 kun";
+//     break;
+//   case 9:
+//     output = "30 kun";
+//     break;
+//   case 10:
+//     output = "31 kun";
+//     break;
+//   case 11:
+//     output = "30 kun";
+//     break;
+//   case 12:
+//     output = "31 kun";
+//     break;
+// }
+
+// alert(output);
+
+// 5-masala
+
+// let a = Number(prompt("Birinchi sonni kirirting :"));
+// let b = Number(prompt("Ikkinchi sonni kirirting :"));
+// let c = prompt("Qaysi operatorni tanlaysiz :");
+
+// let output;
+
+// switch (c) {
+//   case "*":
+//     output = a * b;
+//     break;
+//   case "/":
+//     output = a / b;
+//     break;
+//   case "+":
+//     output = a + b;
+//     break;
+//   case "-":
+//     output = a - b;
+//     break;
+//   default:
+//     output = "topilmadi";
+// }
+
+// console.log(output);
+
+// 6-masala
+
+// let a = Number(
+//   prompt("Uznlik birliklaridan birini tanlang: 1-dm, 2-km, 3-m, 4-mm,5-sm ")
+// );
+// let b = Number(prompt("Enter the number :"));
+// let output;
+
+// switch (a) {
+//   case 1:
+//     output = b * 0.1;
+//     break;
+//   case 2:
+//     output = b * 1000;
+//     break;
+//   case 3:
+//     output = b * 1;
+//     break;
+//   case 4:
+//     output = b * 0.001;
+//     break;
+//   case 5:
+//     output = b * 0.01;
+//     break;
+// }
+
+// alert(output);
+
+// 7-masala
+
+// let a = Number(
+//   prompt("Og'irlik birliklaridan birini tanlang: 1-kg, 2-mg, 3-g, 4-t,5-s ")
+// );
+// let b = Number(prompt("Enter the number :"));
+// let output;
+
+// switch (a) {
+//   case 1:
+//     output = b * 1;
+//     break;
+//   case 2:
+//     output = b * 1000;
+//     break;
+//   case 3:
+//     output = b * 0.001;
+//     break;
+//   case 4:
+//     output = b * 1000;
+//     break;
+//   case 5:
+//     output = b * 100;
+//     break;
+// }
+
+// alert(output);
+
+// 8-masala
+
+// let d = Number(prompt("Day :"));
+// let m = Number(prompt("Month :"));
+
+// let output;
+
+// switch (m) {
+//   case 1:
+//     output = `${d} - yanvar`;
+//     break;
+//   case 2:
+//     output = `${d} - fevral`;
+//     break;
+//   case 3:
+//     output = `${d} - mart`;
+//     break;
+//   case 4:
+//     output = `${d} - aprel`;
+//     break;
+//   case 5:
+//     output = `${d} - may`;
+//     break;
+//   case 6:
+//     output = `${d} - iyun`;
+//     break;
+//   case 7:
+//     output = `${d} - iyul`;
+//     break;
+//   case 8:
+//     output = `${d} - avgust`;
+//     break;
+//   case 9:
+//     output = `${d} - sentabr`;
+//     break;
+//   case 10:
+//     output = `${d} - oktabr`;
+//     break;
+//   case 11:
+//     output = `${d} - noyabr`;
+//     break;
+//   case 12:
+//     output = `${d} - dekabr`;
+//     break;
+// }
+
+// alert(output);
+
+// 9-masala
+
+// let d = Number(prompt("Day :"));
+// let m = Number(prompt("Month :"));
+
+// let output;
+
+// switch (m) {
+//   case 1:
+//     output = `${d + 1} - yanvar`;
+//     break;
+//   case 2:
+//     output = `${d + 1} - fevral`;
+//     break;
+//   case 3:
+//     output = `${d + 1} - mart`;
+//     break;
+//   case 4:
+//     output = `${d + 1} - aprel`;
+//     break;
+//   case 5:
+//     output = `${d + 1} - may`;
+//     break;
+//   case 6:
+//     output = `${d + 1} - iyun`;
+//     break;
+//   case 7:
+//     output = `${d + 1} - iyul`;
+//     break;
+//   case 8:
+//     output = `${d + 1} - avgust`;
+//     break;
+//   case 9:
+//     output = `${d + 1} - sentabr`;
+//     break;
+//   case 10:
+//     output = `${d + 1} - oktabr`;
+//     break;
+//   case 11:
+//     output = `${d + 1} - noyabr`;
+//     break;
+//   case 12:
+//     output = `${d + 1} - dekabr`;
+//     break;
+// }
+
+// alert(output);
