43ff42ef69db6932b50c0080b3cfaf342400d676
Khadicha <246abdurashidova123@gmail.com>
2023-02-13 19:15:44 +0500

    commit 76

--- lesson/lesson7/script.js
+++ lesson/lesson7/script.js
@@ -1,1 +1,50 @@
 "use script";
+
+let arry = [
+  [12, 24, 35, [16, 27, 45], [10, 20, 30]],
+  [33, 43, 46],
+  ["one", "two", "three"],
+  [1000, 2000, 3000],
+];
+
+// console.log(arry[0][0]);
+
+// const user1 = {
+//   fName: "Aziza",
+//   login: "Aziza",
+//   password: "qwerty12345",
+//   email: "something@gmail.com",
+//   phoneNumber: "998 765 43 21",
+//   year: 2005,
+//   now: 2023,
+
+//   place: {
+//     country: "Uzbekistan",
+//     district: "Uchtepa",
+//   },
+
+//   howold: function () {
+//     let a = this.now - this.year;
+//     return a;
+//   },
+// };
+
+// console.log(user1.email);
+// console.log(user1.place.country);
+// console.log(user1.howold());
+// console.log(Object.keys(user1));
+// console.log(Object.values(user1));
+// console.log(Object.entries(user1));
+
+// loops
+
+// for (let a = 1; a <= 10; a++) {
+//   console.log(a);
+//   console.log(a);
+// }
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
