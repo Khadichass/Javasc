08115565b5cc84c582dbef5b40208133bad27d7c
Khadicha <246abdurashidova123@gmail.com>
2023-02-15 16:13:17 +0500

    commit 84

--- practice/practice5/script.js
+++ practice/practice5/script.js
@@ -2,27 +2,51 @@
 
 // 1-masala
 
-let k = Number(prompt("Enter the K number :"));
-let n = Number(prompt("enter the N number :"));
+// let k = Number(prompt("Enter the K number :"));
+// let n = Number(prompt("enter the N number :"));
 
-for (let i = k; i > n; i++) {
-  console.log(i);
-}
-
-// let a = Number(prompt("a:"));
-// let b = Number(prompt("b:"));
-// let c = a + b;
-// console.log(c);
-
-// 2-masala
-
-// let a = 1ater then b (a<b): "));
-// let b = 1ater then b (a<b): "));
-
-// for (let i = a; i < b; i++) {
+// for (let i = k; i > n; i++) {
 //   console.log(i);
 // }
 
+// 2-masala
+// let a = 4;
+// let b = 8;
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+
+// 3-masala
+// let a = 9;
+// let b = 20;
+// for (let i = b - 1; i >= a + 1; --i) {
+//   console.log(i);
+// }
+
+// 4-masala
+
+// let pr = 10;
+// let sum = "";
+// for (let i = 1; i <= 10; i++) {
+//   sum = pr * i;
+//   console.log(sum);
+// }
+
+// 5-masala
+// let pr = 10;
+// let sum = "";
+// for (let i = 0.1; i <= 1; ++i) {
+//   sum = pr * i;
+//   console.log(sum);
+// }
+
+// 6-masala
+let pr = 10;
+let sum = "";
+for (let i = 1.2; i <= 2; i++) {
+  sum = pr * i;
+  console.log(sum);
+}
 // let text = "";
 
 // for (let i = 0; i <  5; i++) {
