43e921e07a7c0c3f50e8be9574062d99b7be689e
Khadicha <246abdurashidova123@gmail.com>
2023-02-15 15:34:38 +0500

    commit 83

--- practice/practice5/script.js
+++ practice/practice5/script.js
@@ -2,8 +2,29 @@
 
 // 1-masala
 
-// let k = Number(prompt("Enter the K number :"));
+let k = Number(prompt("Enter the K number :"));
+let n = Number(prompt("enter the N number :"));
 
-// for (let i = 0; i < 10; i++) {
-//   console.log(k[i]);
+for (let i = k; i > n; i++) {
+  console.log(i);
+}
+
+// let a = Number(prompt("a:"));
+// let b = Number(prompt("b:"));
+// let c = a + b;
+// console.log(c);
+
+// 2-masala
+
+// let a = 1ater then b (a<b): "));
+// let b = 1ater then b (a<b): "));
+
+// for (let i = a; i < b; i++) {
+//   console.log(i);
+// }
+
+// let text = "";
+
+// for (let i = 0; i <  5; i++) {
+//   text += "The number is " + i + "<br>";
 // }
