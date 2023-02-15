d2f3c88586f04dc062818de81683810af526a4f5
Khadicha <246abdurashidova123@gmail.com>
2023-02-15 09:28:11 +0500

    commit 82

--- practice/practice5/script.js
+++ practice/practice5/script.js
@@ -1,3 +1,10 @@
 "use script";
 
 // 1-masala
+
+let k = Number(prompt("Enter the K number :"));
+// let n = Number(prompt("Enter the N number :"))
+
+for (let i = 1; i > 0; i++) {
+  console.log(k[i]);
+}
