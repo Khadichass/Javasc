dafcc7ce1237919c4a3f02a5f16481feb3aabc6b
Khadicha <246abdurashidova123@gmail.com>
2023-02-17 17:04:12 +0500

    commit 92

--- practice/practice6/script.js
+++ practice/practice6/script.js
@@ -43,6 +43,95 @@
 
 // 3-masala
 
-let a = Number(prompt("enter the a number :"));
-let b = Number(prompt("enter the b number :"));
-let c = Number(prompt("enter the c number :"));
+// let a = Number(prompt("enter the a number :"));
+// let b = Number(prompt("enter the b number :"));
+
+// function MEAN(a, b) {
+//   arm = (a + b) / 2;
+//   gem = (a * b) ** 0.5;
+//   console.log(arm, gem);
+//   return arm, gem;
+// }
+
+// MEAN(a, b);
+
+// 4-masala
+
+// let a = Number(prompt("enter the a number :"));
+
+// function trinagle(a) {
+//   p = a * 3;
+//   s = (3 ** 0.5 * a ** 2) / 4;
+//   console.log(p, s);
+//   return p, s;
+// }
+
+// trinagle(a);
+
+// 5-masala
+
+// let x1 = Number(prompt("x1 :"));
+// let x2 = Number(prompt("x2 :"));
+// let y1 = Number(prompt("y1 :"));
+// let y2 = Number(prompt("y2 :"));
+
+// let a = x2 - x1;
+// let b = y2 - y1;
+
+// function Rect(a, b) {
+//   S = a * b;
+//   P = (a + b) * 2;
+//   console.log(P, S);
+//   return P, S;
+// }
+
+// Rect(a, b);
+
+// 6-masala
+
+// let a = Number(prompt("enter the a number :"));
+// let b = Number(prompt("enter the b number :"));
+// let c = Number(prompt("enter the c number :"));
+
+// function Sum(a, b, c) {
+//   sum1 = a + b + c;
+//   console.log(sum1);
+//   return sum1;
+// }
+// Sum(a, b, c);
+
+// 7-masala
+
+// let A = prompt("A :");
+// let B = prompt("B :");
+// let C = prompt("C :");
+
+// let e = A;
+// let f = B;
+// let h = C;
+// function numb(A, B, C) {
+//   A = f;
+//   B = h;
+//   C = e;
+
+//   console.log(B, C, A);
+//   return B, C, A;
+// }
+
+// numb(A, B, C);
+
+// 8-masala
+
+// let k = Number(prompt("Enter the K number :"));
+// let r = Number(prompt("Enter the R number (1<=R<=9)"));
+
+// function AddLeftDigit(k, r) {
+//   if (1 <= r <= 9) {
+//     console.log(k + r);
+//   } else {
+//     console.log("Not found");
+//   }
+//   return k, r;
+// }
+
+// AddLeftDigit(k, r);
