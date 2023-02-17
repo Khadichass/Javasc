2e007dfffd8a5f5f99ef36a2dd9e01b937e545c0
Khadicha <246abdurashidova123@gmail.com>
2023-02-17 16:22:33 +0500

    commit 91

--- practice/practice6/script.js
+++ practice/practice6/script.js
@@ -18,3 +18,31 @@
 //   return a3, b3, c3, d3, e3;
 // }
 // Power(a, b, c, d, e);
+
+// 2-masala
+
+// let a = Number(prompt("enter the a number :"));
+// let b = Number(prompt("enter the b number :"));
+// let c = Number(prompt("enter the c number :"));
+
+// function Power(a, b, c) {
+//   a2 = a ** 2;
+//   a3 = a ** 3;
+//   a4 = a ** 4;
+//   b2 = b ** 2;
+//   b3 = b ** 3;
+//   b4 = b ** 4;
+//   c2 = c ** 2;
+//   c3 = c ** 3;
+//   c4 = c ** 4;
+
+//   console.log(a2, a3, a4, b2, b3, b4, c2, c3, c4);
+//   return a2, a3, a4, b2, b3, b4, c2, c3, c4;
+// }
+// Power(a, b, c);
+
+// 3-masala
+
+let a = Number(prompt("enter the a number :"));
+let b = Number(prompt("enter the b number :"));
+let c = Number(prompt("enter the c number :"));
