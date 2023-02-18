643f76724deb176715a20e10898583d387a8f225
Khadicha <246abdurashidova123@gmail.com>
2023-02-18 12:09:50 +0500

    commit 96

--- lesson/lesson9/script.js
+++ lesson/lesson9/script.js
@@ -1,1 +1,16 @@
 "use script";
+
+// let head1 = document.querySelector(".header1");
+// let head2 = document.querySelector("#header2");
+// let paragrph = document.querySelector("p");
+
+// head1.textContent;
+
+let head1 = document.getElementById(".header1");
+let head2 = document.getElementsByClassName("#header2");
+let paragrph = document.getElementsByTagNameNS("p");
+
+head1.textContent;
+
+console.log(head1.textContent);
+console.log(head2);
