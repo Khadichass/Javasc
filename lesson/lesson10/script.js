5e7e1c59b50cfac0ef68c96cf3dbfbb19af37d07
Khadicha <246abdurashidova123@gmail.com>
2023-02-20 18:19:56 +0500

    commit 106

--- lesson/lesson10/script.js
+++ lesson/lesson10/script.js
@@ -1,1 +1,11 @@
 "use script";
+
+let mainTitle = document.querySelector(".main__left--title");
+let mainDesc = document.querySelector(".main__left--desc");
+let mainMore = document.querySelector(".main__more");
+let mainRight = document.querySelector(".main__right");
+let openBtn = document.getElementById("main__btn");
+let closeBtn = document.getElementById("close__btn");
+
+// mainTitle.textContent =
+//   "Lorem ipsum dolor, sit amet consectetur adipisicing elit";
