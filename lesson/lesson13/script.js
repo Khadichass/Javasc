334d773274af62eda807a79e27ecb57b1e60627f
Khadicha <246abdurashidova123@gmail.com>
2023-02-26 18:15:38 +0500

    commit 110

---
+++ lesson/lesson13/script.js
@@ -0,0 +1,20 @@
+"use script";
+
+let str = "Javascript";
+
+// console.log(str);
+
+// console.log(str.repeat(2));
+
+let str2 = "Javascript \ndeveloper";
+
+// console.log(str2);
+
+// padStart
+
+let str3 = "Java";
+// console.log(str3.padStart(6, "t"));
+
+// padStart
+
+console.log(str3.padEnd(6, "t"));
