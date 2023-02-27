b5db95a74676931ac797aa7cb3d3fdb9f6e12561
Khadicha <246abdurashidova123@gmail.com>
2023-02-27 18:27:15 +0500

    commit 113

--- lesson/lesson14/script.js
+++ lesson/lesson14/script.js
@@ -1,3 +1,19 @@
 "use script";
 
 // call and apply
+
+let user = {
+  fName: "Umar",
+  lName: "Hamidov",
+  login: "umar",
+  pass: "password",
+  email: "email@gmail.com",
+  currenty: 2023,
+
+  countby: function (age) {
+    let birthy = this.currenty - age;
+    return birthy;
+  },
+};
+
+console.log(user.countby(30));
