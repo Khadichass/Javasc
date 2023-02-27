08d5b27a2bc64c333cac4808d21b882511ea7f66
Khadicha <246abdurashidova123@gmail.com>
2023-02-27 18:32:43 +0500

    commit 114

--- lesson/lesson14/script.js
+++ lesson/lesson14/script.js
@@ -2,18 +2,35 @@
 
 // call and apply
 
-let user = {
-  fName: "Umar",
-  lName: "Hamidov",
-  login: "umar",
-  pass: "password",
-  email: "email@gmail.com",
-  currenty: 2023,
+// let user = {
+//   fName: "Umar",
+//   lName: "Hamidov",
+//   login: "umar",
+//   pass: "password",
+//   email: "email@gmail.com",
+//   currenty: 2023,
 
-  countby: function (age) {
-    let birthy = this.currenty - age;
-    return birthy;
-  },
-};
+//   countby: function (age, sum) {
+//     let birthy = this.currenty - age;
+//     return { birthy, sum };
+//   },
+// };
 
-console.log(user.countby(30));
+// console.log(user.countby(30));
+
+// let admin = {
+//   fName: "Umar",
+//   lName: "Hamidov",
+//   login: "umar",
+//   pass: "password",
+//   email: "email@gmail.com",
+//   currenty: 2023,
+// };
+
+// let a = [50, 70];
+
+// let b = 60;
+
+// console.log(user.countby.call(admin, b));
+
+// console.log(user.countby.apply(admin, a));
