30baed7c3fdaaa19f1d3c7db8ec63dd6a98f34a9
Khadicha <246abdurashidova123@gmail.com>
2023-02-27 18:40:43 +0500

    commit 115

--- lesson/lesson14/script.js
+++ lesson/lesson14/script.js
@@ -34,3 +34,38 @@
 // console.log(user.countby.call(admin, b));
 
 // console.log(user.countby.apply(admin, a));
+
+// decloration function
+
+// functName(12, 90);
+
+// function functName(a, b) {
+//   let sum = a + b;
+//   console.log(sum);
+//   return;
+// }
+
+// expression function
+
+// functName = function (a, b) {
+//   let sum = a - b;
+//   console.log(sum);
+//   return;
+// };
+
+// functName(12, 90);
+
+// arrow function
+
+// functName = (a, b) => {
+//   let sum = a * b;
+//   console.log(sum);
+//   return;
+// };
+
+// functName(12, 90);
+
+// let arrowfunct = (a = 1, b = 1) => a / b;
+
+// console.log(arrowfunct(10));
+// console.log(arrowfunct(20, 30));
