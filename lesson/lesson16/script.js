95c1659c36ffc7d80588a74c67e6cc99881d1d2a
Khadicha <246abdurashidova123@gmail.com>
2023-03-03 18:42:44 +0500

    commit 122

--- lesson/lesson16/script.js
+++ lesson/lesson16/script.js
@@ -1,19 +1,40 @@
 "use script";
 
-let object1 = {
-  fNmae: "Hadicha",
-  pass: 1234567,
-  email: "email@gmail.com",
+let user = {
+  fName: "User",
+  login: "user",
+  pass: "passUser",
 };
 
-let object2 = {
-  fNmae: "Hadicha",
-  pass: 1234567,
-  email: "email@gmail.com",
+let admin = {
+  fName: "Admin",
+  login: "admin",
+  pass: "passAdmin",
 };
 
-let object3 = {
-  fNmae: "Hadicha",
-  pass: 1234567,
-  email: "email@gmail.com",
+let customer = {
+  fName: "Customer",
+  login: "customer",
+  pass: "passCustomer",
 };
+
+let arr = [user, admin, customer];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+
+//   if (arr[i].fName == "User") {
+//     console.log(arr[i]);
+//   }
+// }
+
+// let newfind = arr.find((el) => {
+//   return el.fName == "User";
+// });
+// console.log(newfind);
+// console.log(arr);
+
+// let newfind = arr.findIndex((el) => {
+//   return el.fName == "User";
+// });
+// console.log(newfind);
