89c3cf74e5931d89c49a97084427b632674f66c6
Khadicha <246abdurashidova123@gmail.com>
2023-03-03 18:49:12 +0500

    commit 123

---
+++ lesson/htmljs/script.js
@@ -0,0 +1,55 @@
+"use script";
+
+let mainTitle = document.querySelector(".main__left--title");
+let mainDesc = document.querySelector(".main__left--desc");
+let mainMore = document.querySelector(".main__more");
+let mainRight = document.querySelector(".main__right");
+let openBtn = document.getElementById("main__btn");
+let closeBtn = document.getElementById("close__btn");
+
+// mainTitle.textContent =
+//   "Lorem ipsum dolor, sit amet consectetur adipisicing elit";
+
+// openBtn.addEventListener("click", function () {
+//   alert("Hello World");
+//   mainRight.style.backgroundColor = "yellow";
+// });
+
+//----------
+
+// let onClick = function () {
+//   alert("Hello");
+//   mainRight.style.backgroundColor = "yellow";
+// };
+
+// openBtn.addEventListener("click", onClick);
+
+//-----------
+
+let openMenubtn = function () {
+  mainMore.style.display = "block";
+};
+
+openBtn.addEventListener("click", openMenubtn);
+
+let closeMenubtn = function () {
+  mainMore.style.display = "none";
+};
+
+closeBtn.addEventListener("click", closeMenubtn);
+
+let toogle = false;
+let toogleFunc = function () {
+  if (toogle) {
+    toogle = false;
+  } else {
+    toogle = true;
+  }
+};
+
+let toogleBtnFunc = function () {
+  toogleFunc();
+  toogleBtn.style.display = toogle ? "olive" : "red";
+};
+
+toogleBtn.addEventListener("click", toogleBtnFunc);
