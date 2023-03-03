36287ccffdba3a9356d88471e99355897d062bae
Khadicha <246abdurashidova123@gmail.com>
2023-03-03 17:56:40 +0500

    commit 120

---
+++ lesson/lesson16/script.js
@@ -0,0 +1,19 @@
+"use script";
+
+let object1 = {
+  fNmae: "Hadicha",
+  pass: 1234567,
+  email: "email@gmail.com",
+};
+
+let object2 = {
+  fNmae: "Hadicha",
+  pass: 1234567,
+  email: "email@gmail.com",
+};
+
+let object3 = {
+  fNmae: "Hadicha",
+  pass: 1234567,
+  email: "email@gmail.com",
+};
