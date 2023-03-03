7657626c529937a838e9b87c77e0d92c95361a55
Khadicha <246abdurashidova123@gmail.com>
2023-03-03 17:01:53 +0500

    commit 119

--- lesson/lesson15/script.js
+++ lesson/lesson15/script.js
@@ -1,16 +1,99 @@
 "use script";
 
-const arr = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
-let min = arr[0];
+// const arr = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
+// let min = arr[0];
 
-function arrfunct() {
-  for (let i = 0; i < arr.length; i++) {
-    if (min > arr[i]) {
-      min = arr[i];
-    }
-  }
-  return min;
-}
-arrfunct();
+// function arrfunct() {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (min > arr[i]) {
+//       min = arr[i];
+//     }
+//   }
+//   return min;
+// }
+// arrfunct();
 
-console.log(min);
+// console.log(min);
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const arr2 = [11, 12, 13, 14, 15, 16, 17];
+
+// slice immutable
+
+// let newArr = arr.slice(0, 4);
+// console.log(newArr);
+// console.log(arr);
+
+//splice mutable
+
+// let newArr = arr.splice(0, 4);
+// console.log(newArr);
+// console.log(arr);
+
+// reverse mutable
+
+// let newArr = arr.reverse();
+// console.log(newArr);
+// console.log(arr);
+
+// concat immutable
+
+// const newArr = arr.concat(arr2);
+
+// console.log(newArr);
+// console.log(arr);
+// console.log(arr2);
+
+// join immutable
+
+// const str = arr.join();
+// console.log(str);
+// console.log(typeof str);
+// console.log(arr.join());
+// console.log(arr.join(""));
+// console.log(arr.join(" "));
+
+// at
+
+// console.log(arr.length);
+// console.log(arr[arr.length]);
+// console.log(arr[arr.length - 1]);
+// console.log(arr.at(-1));
+
+// for each
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (const element of arr) {
+//   console.log(element);
+// }
+
+// arr.forEach((element) => {
+//   console.log(element);
+//   return element;
+// });
+
+// let newMap = arr.map(function (element) {
+//   console.log(element);
+//   return element * 2;
+// });
+
+// console.log(newMap);
+
+// filter
+
+// let newFilter = arr.filter((el) => el > 5);
+// console.log(newFilter);
+
+// reduce
+// let newReduce = arr.reduce((max, value, index) => {
+//   if (max < value) {
+//     max = value;
+//   }
+//   // console.log(max);
+//   return max;
+// }, arr[0]);
+
+// console.log(newReduce);
