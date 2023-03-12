da035535e66e782603e8c5b92cd10429acccdeee
Khadicha <246abdurashidova123@gmail.com>
2023-03-12 12:29:15 +0500

    Interval created

--- lesson/lesson17/script.js
+++ lesson/lesson17/script.js
@@ -1,12 +1,15 @@
 "use script";
 
-let timeOut = setTimeout(alert("Hello"), 2000);
+// let timeOut = setTimeout(alert("Hello"), 2000);
 
-let time = setTimeout(
-  (funct = (a, b) => {
-    console.log(a + b);
-  }),
-  3000,
-  23,
-  24
-);
+// let time = setTimeout(
+//   (funct = (a, b) => {
+//     console.log(a + b);
+//   }),
+//   3000,
+//   23,
+//   24
+// );
+
+// let interval = setInterval(alert("Hello"), 2000);
+// let interval = setInterval(console.log("Hello"), 1000);
