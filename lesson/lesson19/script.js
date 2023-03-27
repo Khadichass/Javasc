f8833efa4298ab2fc43967787e31cfc5efcecd97
Khadicha <246abdurashidova123@gmail.com>
2023-03-27 18:13:12 +0500

    files created in lesson 20

---
+++ lesson/lesson19/script.js
@@ -0,0 +1,50 @@
+"use script";
+
+// const parentBox = document.getElementById("parentBox");
+// const parentBoxFirstChild = document.getElementsByClassName(
+//   "parentBox__firstChild"
+// );
+// const changeBtn = document.getElementById("changeBtn");
+
+// const createChild = document.createElement("div");
+
+// const changeBtnClicked = () => {
+//   createChild.innerHTML = ` <div class="parentBox" id="parentBox">
+//   <h2 class="parentBox__firstChild">
+//     Boshlang'ich holatdagi parentga nesting bo'lgan element
+//   </h2>
+// </div>`;
+// createChild.innerHTML = "<p>lorem</p>";
+// parentBox.before(createChild);
+// parentBox.after(createChild);
+// parentBox.append(createChild);
+// parentBox.prepend(createChild);
+// parentBox.classList.add("addClass");
+// document.querySelector(".parentBox__firstChild");
+// parentBox.classList.remove("parentBox");
+// parentBox.classList.toggle("addClass");
+// const containClass = parentBox.classList.contains("parentBox");
+// alert(containClass);
+// window.scrollTo({ top: "0px", behavior: "smooth" });
+// };
+
+// changeBtn.addEventListener("click", changeBtnClicked);
+
+// const onMouseEnter = () => {
+//   parentBox.classList.add("addClass");
+//   changeBtn.removeEventListener("mouseenter", onMouseEnter);
+// };
+
+// changeBtn.addEventListener("mouseenter", onMouseEnter);
+
+// changeBtn.addEventListener("keydown", function (a) {
+//   if (a.key == "Enter") {
+//     parentBox.classList.toggle("addClass");
+//   }
+// });
+
+// document.addEventListener("keydown", function (event) {
+//   if (event.code === "Enter") {
+//     parentBox.classList.toggle("addClass");
+//   }
+// });
