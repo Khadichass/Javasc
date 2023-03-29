b648ddf2976495327391a19b092fb0b88a98f7f0
Khadicha <246abdurashidova123@gmail.com>
2023-03-29 11:15:28 +0500

    commit 105

--- lesson/lesson20/script.js
+++ lesson/lesson20/script.js
@@ -13,3 +13,29 @@ const stars = document.querySelector(".stars_paragraph");
 const watchers = document.querySelector(".watchers_paragraph");
 const forks = document.querySelector(".forks_paragraph");
 const getBtn = document.getElementById("getBtn");
+
+getBtn.addEventListener("click", onClick);
+
+function onClick() {
+  console.log(search.value);
+  getData();
+}
+
+const getData = async function () {
+  try {
+    const dataJson = await fetch(
+      `https://api.github.com/users/${search.value}?client_id=d487070e8ebeb53eb623&client_secret=f231d73655a740d44c8f87a2e9df95e1fa919385`
+    );
+
+    const data = await dataJson.json();
+    console.log(dataJson);
+    console.log(data);
+    console.log(data.company);
+    console.log(data.bio);
+    console.log(data.company);
+    console.log(data.company);
+    console.log(data.company);
+  } catch (error) {
+    return error;
+  }
+};
